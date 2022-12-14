2aabe0a950068db0a929077249eedce0bc1ce687
ecraw24 <elcrawford24@gmail.com>
2022-12-13 21:28:19 -0600

    working prototype for test file

--- app.py
+++ app.py
@@ -5,12 +5,38 @@ import os
 from werkzeug.utils import secure_filename
 import Unzip
 import sql
+import shutil 
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'Uploads/'
+HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
+fonts = ['Amaranth', 'Arial', 'Caudex', 'Courier New', 'Frutiger Linotype', 'Gentium Basic', 'Georgia', 'Istok Web', 
+                'Josefin Sans', 'Puritan', 'Tahoma', 'Times New Roman', 'Trebuchet MS', 'Ubuntu', 'Verdana']
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
+    #if request.method == 'GET':
+    folder = 'extractedDBs' 
+    for filename in os.listdir(folder):
+      file_path = os.path.join(folder, filename)
+      try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+      except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+    folder = 'Uploads' 
+    for filename in os.listdir(folder):
+      file_path = os.path.join(folder, filename)
+      try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+      except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+    
     return render_template('index.html')
 
 @app.route('/success', methods = ['POST'])  
@@ -21,7 +47,19 @@ def success():
         f.save(FilePath) 
         dbFilePath = Unzip.extractDB(FilePath)
         tableList = sql.printTables(dbFilePath)
-        return render_template("uploadSuccess.html", name = f.filename, table=tableList)  
+        
+        return render_template("uploadSuccess.html", name = dbFilePath, table=tableList, fonts=fonts, complete='')  
+
+@app.route('/fontChange', methods = ['POST', 'GET'])
+def fontChange():
+  if request.method == 'POST':
+    sql.changeFonts(request.form['fontFrom'], request.form['fontTo'], 'extractedDBs\WordPower60 SS_Copy.db')
+    return render_template("downloadFile.html")
+
+@app.route('/download')
+def download():
+    path = 'extractedDBs\WordPower60 SS_Copy.db'
+    return send_file(path, as_attachment=True)   
 
 if __name__ == "__main__":
   app.run()
--- sql.py
+++ sql.py
@@ -2,8 +2,6 @@
 import sqlite3
 
 def printTables (dbFilePath):
-    # creating file path
-    #dbfile = r'C:\Users\wydnerk\OneDrive\Personal\My Projects\AACFileChanger\WordPower60 SS\WordPower60 SS_Copy.db'
     # Create a SQL connection to our SQLite database
     con = sqlite3.connect(dbFilePath)
     print(dbFilePath)
@@ -12,15 +10,18 @@ def printTables (dbFilePath):
     cur = con.cursor()
 
     # reading all table names
-    #table_list = [a for a in cur.execute("SELECT * FROM button_styles")]
     table_list = [a for a in cur.execute("SELECT name from sqlite_master where type= 'table'")]
-    #table_list = cur.fetchall()
-    #(table_list)
-
-    # here is you table list
-    #print(table_list)
 
     # Be sure to close the connection
     con.close()
 
     return table_list
+
+def changeFonts (fontFrom, fontTo, dbFilePath):
+    con = sqlite3.connect(dbFilePath)
+    print(dbFilePath)
+    cur = con.cursor()
+
+    [a for a in cur.execute("update button_styles set font_name='" + fontTo + "' where font_name='" + fontFrom + "'")]
+
+    con.close()
